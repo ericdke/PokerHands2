48a35debd448d178474f4df3b253b24fdd6916bc
Eric Dejonckheere <eric@aya.io>
2022-01-20 18:45:05 +0100

    headsup result

---
+++ PokerHands/Source/Main/SPHSidebar.swift
@@ -0,0 +1,91 @@
+//
+//  SPHSidebar.swift
+//  PokerHands
+//
+//  Created by Eric Dejonckheere on 20/01/2022.
+//
+
+import SwiftUI
+
+struct SPHSidebar: View {
+    
+    @EnvironmentObject var model: SPHDemoModel
+    var width: CGFloat
+    
+    var body: some View {
+        VStack {
+            VStack(alignment: .leading) {
+                Text("Player 1")
+                
+                TextField("enter name", text: $model.player1Name)
+            }
+            .padding()
+            .background(Color.blue)
+            
+            VStack(alignment: .leading) {
+                Text("Player 2")
+                
+                TextField("enter name", text: $model.player2Name)
+            }
+            .padding()
+            .background(Color.pink)
+            
+            VStack(alignment: .leading) {
+                Text("Rounds")
+                
+                HStack {
+                    TextField("100", text: $model.numberOfRoundsLabel)
+                    
+                    Button("Go") {
+                        // do it
+                    }
+                    .background(Color.green)
+                }
+            }
+            .padding()
+            .background(Color.purple)
+            
+            VStack {
+                // random or custom cards
+            }
+            
+            Spacer()
+            
+            VStack {
+                VStack {
+                    Text(model.p1name)
+                        .fontWeight(.heavy)
+                        .multilineTextAlignment(.leading)
+                        .lineLimit(2)
+                        .padding(.bottom)
+                        .frame(width: width)
+                    
+                    Text("\(model.player1Score) wins")
+                }
+                .padding(.vertical)
+                .background(Color.blue)
+
+                VStack {
+                    Text(model.p2name)
+                        .fontWeight(.heavy)
+                        .multilineTextAlignment(.leading)
+                        .lineLimit(2)
+                        .padding(.bottom)
+                        .frame(width: width)
+                    
+                    Text("\(model.player2Score) wins")
+                }
+                .padding(.vertical)
+                .background(Color.pink)
+            }
+            .font(.title)
+        }
+        .frame(width: width)
+    }
+}
+
+struct SPHSidebar_Previews: PreviewProvider {
+    static var previews: some View {
+        SPHSidebar(width: 220).environmentObject(SPHDemoModel())
+    }
+}
