49befeba4d6df0a385e1c6b4cb4967a1cdf4857f
Eric Dejonckheere <eric@aya.io>
2022-01-23 14:08:56 +0100

    package 2nd version

--- Package.swift
+++ Package.swift
@@ -23,6 +23,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TexasPokerHands",
-            path: "PokerHands/TexasPokerHands"),
+            path: "TexasPokerHands"),
     ]
 )
