f623c95d198901fad67512d72f2762b6aeddce04
Eric Dejonckheere <eric@aya.io>
2022-01-23 14:44:57 +0100

    version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "TexasPokerHands",
     platforms: [
-        .macOS(.v11), .iOS(.v15)
+        .macOS(.v11), .iOS(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
