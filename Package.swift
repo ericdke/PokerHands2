a94a2e0e1bdac1c702da28a84c43ff74280e84e8
Eric Dejonckheere <eric@aya.io>
2022-01-23 14:10:42 +0100

    package 3rd version

--- Package.swift
+++ Package.swift
@@ -23,6 +23,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TexasPokerHands",
-            path: "TexasPokerHands"),
+            dependencies: []),
+        .testTarget(
+            name: "TexasPokerHandsTests",
+            dependencies: ["TexasPokerHands"]),
     ]
 )
